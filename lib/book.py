59e4aa7baf4a22bcd18d5a17a70b76f1d2cea2a9
Tedd James <tedd.james@student.moringaschool.com>
2025-05-08 14:23:26 +0300

    Putting It All Together: Object-Oriented Programming Lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,29 @@
-#!/usr/bin/env python3
+# book.py
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        # store title normally
+        self.title = title
+        # initialize a “backing” attribute for page_count,
+        # then delegate to the setter to validate/type‐check
+        self._page_count = None
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        # the getter simply returns the backing attribute
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        # validate that the new page count is an integer
+        if not isinstance(value, int):
+            # if it’s not, print the required error message
+            print("page_count must be an integer")
+        else:
+            # otherwise store it
+            self._page_count = value
+
+    def turn_page(self):
+        # whenever someone calls this method, print the expected text
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,29 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        # store brand directly
+        self.brand = brand
+
+        # set up a backing attribute for size, then call the setter
+        self._size = None
+        self.size = size
+
+        # condition will be assigned when cobble() is called
+        self.condition = None
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            # exactly matches test expectation
+            print("size must be an integer")
+        else:
+            self._size = value
+
+    def cobble(self):
+        # set condition to 'New' and announce repair
+        self.condition = "New"
+        # match test's expected output exactly
+        print("Your shoe is as good as new!")
